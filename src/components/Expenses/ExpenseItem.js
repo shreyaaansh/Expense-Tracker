65e139203dda255a576d195a03daa4123efc6d44
Shreyansh Jaiswal <jshreyansh459@gmail.com>
2022-10-03 00:18:35 +0530

    Forms Added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,28 +7,17 @@ import ExpenseDate from './ExpenseDate';
 
 import Card from '../UI/Card';
 
-const ExpenseItem = (props) =>{
+const ExpenseItem = (props) => {
 
-    const [newTitle, setNewTitle] = useState("hy"); 
-
-    const [title, setTitle] = useState(props.title); 
-
-    const clickHandler = () => {
-        setTitle('New Title...')
-    }
 
     return (
         <Card className='expense-item'>   
             <ExpenseDate date = {props.date}/>
-
-            <div className='expense-item__description'> 
-                <h2>{ title }</h2>
+            <div className ='expense-item__description'> 
+                <h2>{ props.title }</h2>
                 <div className='expense-item__price'>${ props.amount}</div>
-
             </div>
 
-            <input type="text" value ={newTitle} />
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
